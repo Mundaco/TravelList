395f9e2aef5e8d8e92650380392be304aaed79b0
mnu <>
2019-10-22 17:37:38 +0200

    Menu Save Button

--- app/src/main/java/com/example/travellist/TravelEditActivity.kt
+++ app/src/main/java/com/example/travellist/TravelEditActivity.kt
@@ -4,7 +4,8 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
+import android.view.Menu
+import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_travel_edit.*
 
 
@@ -24,16 +25,22 @@ class TravelEditActivity : AppCompatActivity() {
         }
     }
 
-    fun onClick(view: View?) {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.activity_travel_edit, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
-        if(view == btnSave) {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+
+        if(item.itemId == R.id.menu_save) {
 
             val data = Intent()
             data.putExtra("Position", intent.getIntExtra("Position", -1))
             val info = TravelInfo(
                     "${txtCity.text}",
                     "${txtCountry.text}",
-                     try { "${txtYear.text}".toInt() } catch (e: Exception) { 1900 },
+                    try { "${txtYear.text}".toInt() } catch (e: Exception) { 1900 },
                     "${txtNote.text}"
             )
             data.putExtra("Info", info.toBundle())
@@ -41,5 +48,7 @@ class TravelEditActivity : AppCompatActivity() {
             setResult(Activity.RESULT_OK,data)
             finish()
         }
+
+        return super.onOptionsItemSelected(item)
     }
 }
